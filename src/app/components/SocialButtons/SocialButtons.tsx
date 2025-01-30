f6f18a1136badfbbd0ab29bc6cbf2a3f1e6844e2
ErenElagz <erenelagoz@gmail.com>
2025-01-30 21:41:26 +0300

    Add social buttons component and update styles; include @iconify/react dependency

---
+++ src/app/components/SocialButtons/SocialButtons.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Icon } from "@iconify/react";
+
+function SocialButtons(props) {
+  return (
+    <div className="flex bg-[#211122] rounded-[999] p-3 gap-3 items-center justify-center">
+      <Icon icon={props.icon} width="32" height="32" />
+
+      <a
+        href={props.url}
+        target="_blank"
+      >
+        <p className="text-xl">{props.title}</p>
+      </a>
+
+      <Icon icon="ep:top-right" width="24" height="24" />
+    </div>
+  );
+}
+
+SocialButtons.propTypes = {
+    title: PropTypes.string,
+    url: PropTypes.string,
+    icon : PropTypes.string
+
+};
+
+export default SocialButtons;
