056b051e764a56ca89ce3bf401c6c10aae4b105f
ErenElagz <erenelagoz@gmail.com>
2025-02-09 13:46:50 +0300

    Add new blog entry on optimizing performance in React Native apps

--- data/blogs.ts
+++ data/blogs.ts
@@ -1,6 +1,19 @@
 export const blogs = [
   {
     id: 1,
+    title: "10 Essential Tips for Optimizing Performance in React Native Apps",
+    date: "2025-02-09",
+    mediumUrl:
+      "https://erenelagz.medium.com/10-essential-tips-for-optimizing-performance-in-react-native-apps-8dfc21d9504a",
+    devtoUrl:
+      "https://dev.to/erenelagz/10-essential-tips-for-optimizing-performance-in-react-native-apps-4j93",
+    image:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*0M4NTjXngkYRHEwhpQdUUQ.png",
+    description:
+      "Here are ten essential tips to optimize your React Native app and ensure a smooth user experience.",
+  },
+  {
+    id: 2,
     title: "🚀 Top 10 React Native UI Libraries for Stunning Mobile Apps ",
     date: "2025-02-05",
     mediumUrl:
@@ -13,7 +26,7 @@ export const blogs = [
       " Top 10 React Native UI libraries based on features, ease of use, customization, and community support.",
   },
   {
-    id: 1,
+    id: 3,
     title: "Best React Native Map Libraries, Comparison of the 7 Map Module ",
     date: "2025-02-02",
     mediumUrl:
