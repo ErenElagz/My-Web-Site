c884f0230ba154013cf535fe296ef3bb1c200c4e
ErenElagz <erenelagoz@gmail.com>
2025-02-28 14:08:05 +0100

    Changed URL

--- components/Index/Spotlight.tsx
+++ components/Index/Spotlight.tsx
@@ -16,7 +16,7 @@ export default function Spotlight() {
       <div className="flex md:flex-row gap-4 box-border flex-col">
         {/* Company Os */}
         <a
-          href="https://github.com/ErenElagz/company-os"
+          href="https://erenelagoz.gumroad.com/l/company-os"
           className="group w-full border border-[--navBarBorder] bg-[#ffffff05] rounded-3xl flex flex-col p-5 pt-7 ustify-between"
         >
           <div>
